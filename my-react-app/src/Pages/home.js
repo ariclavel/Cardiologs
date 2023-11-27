be30b5189a925c697fe474b1761acb2607633a59
ariclavel <a01706629@itesm.mx>
2023-11-27 21:50:03 +0100

    Creating web Page

---
+++ my-react-app/src/Pages/home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Button from "../components/Button";
+import {useNavigate} from "react-router-dom";
+import { Box} from '@mui/system';
+const Home = () =>{
+ 
+    return(
+        <div style={{ 
+            backgroundColor: "black" 
+          }}>
+            <Box sx={{ color: '#ffffff', fontSize: 34, fontWeight: 'medium',fontFamily: 'Elephant', padding: 4}}>
+                Ariana Ayaviri Clavel
+            </Box>
+            <div>
+            
+        
+            </div>
+            
+          </div>
+        
+
+    )
+
+};
+export default Home;
